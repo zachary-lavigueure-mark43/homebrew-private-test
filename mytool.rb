8ba5b99b1d21d5cb7fec4a8f6f3aab7b8c60ad14
zachary-lavigueure-mark43 <zachary.lavigueure@mark43.com>
2024-09-03 10:56:05 -0700

    iterative piush

--- mytool.rb
+++ mytool.rb
@@ -1,10 +1,11 @@
 require "formula"
 require_relative "../custom_download_strategy.rb"
 
-class Wget < Formula
+class Mytool < Formula
   homepage ""
   url "https://github.com/zachary-lavigueure-mark43/homebrew-private-test/blob/main/mytool.rb", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
   sha256 "983f7fe3daf96e4c89c0c1cd90bcde9ef9938fc8ee300379f7f361aacca3f09f"
+  
 
   def install
     system "./configure", "--prefix=#{prefix}"
